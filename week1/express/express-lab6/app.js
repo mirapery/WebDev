30ab9aa5c05e84aa90d4debfb438df16d8940726
Mira <mira.peryshko@metropolia.fi>
2024-10-26 01:44:52 +0300

    finished mock server

---
+++ week1/express/express-lab6/app.js
@@ -0,0 +1,24 @@
+// TASK: Create a Mock Server
+
+// 1. npm init -y
+// 2. npm install express
+// 3. create controller file
+
+
+const express = require("express");
+const app = express();
+const port = 3001;
+
+// Import callbacks
+const { getEndpoint1, getEndpoint2, getEndpoint3, getRoot } = require("./controllers");
+
+// Define routes
+app.get("/", getRoot);
+app.get("/endpoint1", getEndpoint1);
+app.get("/endpoint2", getEndpoint2);
+app.get("/endpoint3", getEndpoint3);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`The server is running at http://localhost:${port}`);
+});
